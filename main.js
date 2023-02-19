85db48fa648dca416cc8f9a4523c3bfd2dff202c
lindenkron <lindenkron@hotmail.com>
2023-02-19 04:57:11 +0100

    More fixes on a range of things. Mostly CSS & html

--- main.js
+++ main.js
@@ -2889,7 +2889,7 @@ async function main(){ // main asyncronous thread; mostly initializes the user s
 		`;
 		document.head.appendChild(styleTmp);
 	}
-	getById("credits").innerHTML = "Version: " + session.version + " - " + getById("credits").innerHTML;
+	getById("credits").innerHTML = "Version: " + session.version + " | " + getById("credits").innerHTML;
 	
 	if (urlParams.has('ssb') || urlParams.has('screensharebutton')) {
 		session.screensharebutton = true;
@@ -3901,7 +3901,8 @@ async function main(){ // main asyncronous thread; mostly initializes the user s
 		if (session.webcamonly == true) { // mobile or manual flag 'webcam' pflag set
 			getById("head1").innerHTML = '';
 		} else {
-			getById("head1").innerHTML = '<span style="color:#CCC" data-translate="please-select-option-to-join">Please select an option to join.</span>';
+			getById("head1").getElementsByClassName("left")[0].innerHTML = '<span style="color:#CCC" data-translate="please-select-option-to-join">Please select an option to join.</span>';
+			// getById("head1").innerHTML = '<span style="color:#CCC" data-translate="please-select-option-to-join">Please select an option to join.</span>';
 		}
 
 		if (session.roomid.length > 0) {
@@ -3946,7 +3947,7 @@ async function main(){ // main asyncronous thread; mostly initializes the user s
 				
 			};
 			joinRoom(session.roomid); // this is a scene, so we want high resolutions
-			getById("main").style.overflow = "hidden";
+			// getById("main").style.overflow = "hidden"; 
 
 			if (session.chatbutton === true) {
 				getById("chatbutton").classList.remove("hidden");
